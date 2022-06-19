eb7bf0c56b4f3b16c202bd10a772fc8734cd4f2c
Denislav Rashkov <70910384+deennyy@users.noreply.github.com>
2022-06-19 23:15:23 +0300

    fix indentation

--- server/api/login.php
+++ server/api/login.php
@@ -9,42 +9,42 @@ if (strcmp($api_key, 'REDACTED') != 0) {
 $status = '';
 $username = $_GET['username'];
 if (strlen($username) == 0 || strlen($username) > 20) {
-    $status = 'username must be between 1-20 characters';
+	$status = 'username must be between 1-20 characters';
 } else {
-    $password = $_GET['password'];
-    if (strlen($password) == 0 || strlen($password) > 32) {
-        $status = 'password must be between 1-32 characters';
-    } else {
-        require_once('../connect.php');
-        $query = $sql->query('SELECT * FROM users WHERE username=?', [$username]);
-        if (mysqli_num_rows($query) == 0) {
-            $status = 'incorrect username/password';
-        } else {
-            if (password_verify($password, $query->fetch_assoc()['password'])) {
-		    	$query = $sql->query('SELECT uid FROM users WHERE username=?', [$username]);
-		    	$uid = NULL;
-		    	$rows = $query->fetch_all(1);
-		    	foreach ($rows as $row) {
+	$password = $_GET['password'];
+	if (strlen($password) == 0 || strlen($password) > 32) {
+		$status = 'password must be between 1-32 characters';
+	} else {
+		require_once('../connect.php');
+		$query = $sql->query('SELECT * FROM users WHERE username=?', [$username]);
+		if (mysqli_num_rows($query) == 0) {
+			$status = 'incorrect username/password';
+		} else {
+			if (password_verify($password, $query->fetch_assoc()['password'])) {
+				$query = $sql->query('SELECT uid FROM users WHERE username=?', [$username]);
+				$uid = NULL;
+				$rows = $query->fetch_all(1);
+				foreach ($rows as $row) {
 					$uid = $row['uid'];
-		    	}
-		    	$query = $sql->query('SELECT * FROM tokens WHERE uid=?', [$uid]);
-		    	if (mysqli_num_rows($query) != 0) {
+				}
+				$query = $sql->query('SELECT * FROM tokens WHERE uid=?', [$uid]);
+				if (mysqli_num_rows($query) != 0) {
 					$sql->query('DELETE FROM tokens WHERE uid=?', [$uid]);
-		    	}
-		    	$token = rand();
-		    	$token_hash = md5($token);
-		    	$expires = date('Y-m-d H:i:s', time() + 120);
-		    	$sql->query('INSERT INTO tokens (uid, token, expires) VALUES (?,?,?)', [$uid, $token_hash, $expires]);
-		    	$query = $sql->query('SELECT * FROM tokens WHERE uid=?', [$uid]);
-		    	if (mysqli_num_rows($query) != 1) {
+				}
+				$token = rand();
+				$token_hash = md5($token);
+				$expires = date('Y-m-d H:i:s', time() + 120);
+				$sql->query('INSERT INTO tokens (uid, token, expires) VALUES (?,?,?)', [$uid, $token_hash, $expires]);
+				$query = $sql->query('SELECT * FROM tokens WHERE uid=?', [$uid]);
+				if (mysqli_num_rows($query) != 1) {
 					$status = 'failed generating token';
-		    	} else {
+				} else {
 					$status = 'OK' . $token_hash;
-		    	}
-            } else {
-                $status = 'incorrect username/password';
-            }
-        }
-    }
+				}
+			} else {
+				$status = 'incorrect username/password';
+			}
+		}
+	}
 }
 echo $status;
